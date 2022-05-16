95c884415e47e887e6141c75865dc89f820fd8aa
Santiago <santiago.folgueras@cern.ch>
2022-05-16 16:57:08 +0200

    Add L1TPhase2 Muon DQM code

--- DQMOffline/L1Trigger/python/L1TPhase2Offline_cfi.py
+++ DQMOffline/L1Trigger/python/L1TPhase2Offline_cfi.py
@@ -134,7 +134,11 @@ l1tPhase2CorrelatorOfflineDQM = DQMEDAnalyzer(
 
 )
 
+from DQMOffline.L1Trigger.L1TPhase2MuonOffline_cfi import *
+
 l1tPhase2OfflineDQM = cms.Sequence(
                           l1tPhase2CorrelatorOfflineDQM +
-                          OuterTrackerTkMET
+                          OuterTrackerTkMET +
+                          l1tPhase2MuonOffline
                           )
+
