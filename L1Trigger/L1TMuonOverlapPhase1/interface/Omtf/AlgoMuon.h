d064ad53d4e5e5c0acfe7a3cae78e268b277fc65
Karol Bunkowski <karol.bunkowski@cern.ch>
2024-02-12 19:38:21 +0100

    New version of the pT regression neural network
    The NN is trained on the top of the phase-2 OMTF pattern logic.
    
    LutNeuronLayerFixedPoint:
    added template parameter output_F - this allows to have better precision
    of the lut out sum calculation, and thus better precision of the next
    layer input.
    runWithInterpolation - fixed protection against overflow of the LUT
    address,
    getOutWithOffset() -  fixed protection against overflow of the output
    value.
    
    LutNetworkFixedPointRegression2Outputs - added template parameters
    layer1_output_F,  layer3_input_F.
    
    pt calibration with linear scaling of the output pt instead of
    ptCalibrationArray.
    
    PtAssignmentNNRegression.cc - different bit precision for the NN than in
    the previous version of the NN.
    
    omtfHitWithQualToEventInput() - DT stubs quality is used for the NN
    input.
    
    L1TMuonOverlapPhase1/src/Omtf/OMTFProcessor.cc - setting the output
    unconstrained pT to 1 when getPdfSumConstr.
    
    applied scram build code-checks and scram build code-format

--- L1Trigger/L1TMuonOverlapPhase1/interface/Omtf/AlgoMuon.h
+++ L1Trigger/L1TMuonOverlapPhase1/interface/Omtf/AlgoMuon.h
@@ -40,10 +40,7 @@ public:
   int getBx() const { return m_bx; }
 
   //hardware pt
-  int getPtConstr() const {
-    //TODO maybe it should return 0 and not -1?
-    return goldenPaternConstr == nullptr ? -1 : goldenPaternConstr->key().thePt;
-  }
+  int getPtConstr() const { return goldenPaternConstr == nullptr ? 0 : goldenPaternConstr->key().thePt; }
 
   //hardware upt, in the phase1 the upt scale unit is 1 GeV, while for the pt the unit is 0.5GeV
   int getPtUnconstr() const {
@@ -55,7 +52,7 @@ public:
 
   int getPhiRHit() const { return gpResultConstr.getRefHitPhi(); }
 
-  unsigned int getPatternNum() const ;
+  unsigned int getPatternNum() const;
 
   unsigned int getPatternNumConstr() const {
     return goldenPaternConstr == nullptr ? 0 : goldenPaternConstr->key().theNumber;
